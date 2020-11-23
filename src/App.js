cf1160301b701a75d3348a0a191cfbb963e81890
tatjama <tanja120a@gmail.com>
2020-11-23 17:24:49 +0100

    9. React Router

--- src/App.js
+++ src/App.js
@@ -2,16 +2,28 @@
 import GlobalStyle from './components/GlobalStyle';
 //Pages import
 import AboutUs from './pages/AboutUs';
+import OurWork from './pages/OurWork';
+import ContactUs from './pages/ContactUs';
 import Nav from './components/Nav';
+//Router
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle/>
-      <Nav/>
-      <h1>Film</h1>
-      
-      <AboutUs/>
+      <Nav/>  
+      <Switch>
+        <Route path = "/" exact>
+            <AboutUs/> 
+        </Route>
+        <Route path = "/work">
+          <OurWork/> 
+        </Route>
+        <Route path = "/contact">
+          <ContactUs/>
+        </Route>         
+      </Switch>    
     </div>
   );
 }
