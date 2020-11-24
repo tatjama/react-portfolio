101317af93492d3898e093dc1bbdf6a4f57174cc
tatjama <tanja120a@gmail.com>
2020-11-24 12:31:50 +0100

    14. About Animation

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -3,6 +3,7 @@ import React from 'react';
 import home1 from '../img/home1.png';
 // Animation
 import {motion} from 'framer-motion';
+import {titleAnimation, fade, imageAnimation } from '../animation';
 //Styled
 import {About, Description, Image, Hide} from '../styles';
 
@@ -12,22 +13,22 @@ const AboutSection = () => {
             <Description>
                 <motion.div className = "title">
                     <Hide>
-                        <motion.h2>We work to make</motion.h2>
+                        <motion.h2 variants = {titleAnimation}>We work to make</motion.h2>
                     </Hide>
                     <Hide>
-                        <motion.h2> your <span>dreams</span> come</motion.h2>
+                        <motion.h2 variants = {titleAnimation}> your <span>dreams</span> come</motion.h2>
                     </Hide>
                     <Hide>
-                        <motion.h2> true.</motion.h2>
+                        <motion.h2 variants = {titleAnimation}> true.</motion.h2>
                     </Hide>
                 </motion.div>                
-                <p>Contact us for every photography, videography idea that you have. We have professionals
+                <motion.p variants = {fade}>Contact us for every photography, videography idea that you have. We have professionals
                     with amazing skills.
-                </p>
-                <button>Contact Us</button>
+                </motion.p>
+                <motion.button variants = {fade}>Contact Us</motion.button>
             </Description>
             <Image>
-                <img src = {home1} alt = "gay-with-camera"/>
+                <motion.img variants = {imageAnimation} src = {home1} alt = "gay-with-camera"/>
             </Image>
         </About>
     )
