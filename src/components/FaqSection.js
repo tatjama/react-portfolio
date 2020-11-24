bc3a08411ff41b35d973d31e72ba7da8dbf19917
tatjama <tanja120a@gmail.com>
2020-11-24 20:59:51 +0100

    17.  Reusabille component

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Toggle from './Toggle';
 //Style
 import styled from 'styled-components';
 import {About} from '../styles';
@@ -7,39 +8,46 @@ const FaqSection = () => {
     return(
         <Faq>
             <h2>Any Questions <span>FAQ</span></h2>
-            <div className = "question">
-                <h4>How to start?</h4>
-                <div className = "answer" >
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum, dolor sit amet conscetetur adipisic Necessitatibus, neque.</p>
+            <Toggle>
+                <div className = "question">
+                    <h4>How to start?</h4>
+                    <div className = "answer" >
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>Lorem ipsum, dolor sit amet conscetetur adipisic Necessitatibus, neque.</p>
+                    </div>
+                    <div className = "faq-line"></div>
                 </div>
-                <div className = "faq-line"></div>
-            </div>
-            <div className = "question">
-                <h4>Daily Schedule.</h4>
-                <div className = "answer" >
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum, dolor sit amet conscetetur adipisic Necessitatibus, neque.</p>
+            </Toggle>
+            <Toggle>
+                <div className = "question">
+                    <h4>Daily Schedule.</h4>
+                    <div className = "answer" >
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>Lorem ipsum, dolor sit amet conscetetur adipisic Necessitatibus, neque.</p>
+                    </div>
+                    <div className = "faq-line"></div>
                 </div>
-                <div className = "faq-line"></div>
-            </div>
-            <div className = "question">
-                <h4>Different payment methods.</h4>
-                <div className = "answer" >
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum, dolor sit amet conscetetur adipisic Necessitatibus, neque.</p>
+            </Toggle>
+            <Toggle>
+                <div className = "question">
+                    <h4>Different payment methods.</h4>
+                    <div className = "answer" >
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>Lorem ipsum, dolor sit amet conscetetur adipisic Necessitatibus, neque.</p>
+                    </div>
+                    <div className = "faq-line"></div>
                 </div>
-                <div className = "faq-line"></div>
-            </div>
-            <div className = "question">
-                <h4>What products do you offer?</h4>
-                <div className = "answer" >
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum, dolor sit amet conscetetur adipisic Necessitatibus, neque.</p>
+            </Toggle>
+            <Toggle>
+                <div className = "question">
+                    <h4>What products do you offer?</h4>
+                    <div className = "answer" >
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>Lorem ipsum, dolor sit amet conscetetur adipisic Necessitatibus, neque.</p>
+                    </div>
+                    <div className = "faq-line"></div>
                 </div>
-                <div className = "faq-line"></div>
-            </div>
-            
+            </Toggle>
 
         </Faq>
     )
---
+++ src/components/Toggle.js
@@ -0,0 +1,13 @@
+import React , {useState} from 'react';
+
+const Toggle = ({ children }) => {
+const [toggle, setToggle] = useState(true)
+
+    return(
+        <div onClick = { () => setToggle(!toggle) }>
+            {toggle ? children : ""}
+        </div>
+    )
+}
+
+export default Toggle;
