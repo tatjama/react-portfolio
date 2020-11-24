cb74677f8359b07984e9c944541a5a67d07eef7f
tatjama <tanja120a@gmail.com>
2020-11-24 08:39:44 +0100

    12. framer motion

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,25 +1,26 @@
 import React from 'react';
 //Sections import
 import home1 from '../img/home1.png';
+// Animation
+import {motion} from 'framer-motion';
 //Styled
-//import styled from 'styled-components';
 import {About, Description, Image, Hide} from '../styles';
 
 const AboutSection = () => {
     return(
         <About>            
             <Description>
-                <div className = "title">
+                <motion.div className = "title">
                     <Hide>
-                        <h2>We work to make</h2>
+                        <motion.h2>We work to make</motion.h2>
                     </Hide>
                     <Hide>
-                        <h2> your <span>dreams</span> come</h2>
+                        <motion.h2> your <span>dreams</span> come</motion.h2>
                     </Hide>
                     <Hide>
-                        <h2> true.</h2>
+                        <motion.h2> true.</motion.h2>
                     </Hide>
-                </div>                
+                </motion.div>                
                 <p>Contact us for every photography, videography idea that you have. We have professionals
                     with amazing skills.
                 </p>
