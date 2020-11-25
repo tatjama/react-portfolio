bad7dcb7f7617f50aa417a1bccb01a60a10a9a94
tatjama <tanja120a@gmail.com>
2020-11-25 09:12:10 +0100

    21. Contact Animation

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,15 +1,72 @@
 import React from 'react';
 //Animations
 import { motion } from 'framer-motion';
-import {pageAnimation} from '../animation';
+import {pageAnimation, titleAnimation} from '../animation';
+//Styled
+import styled from 'styled-components';
 
 const ContactUs = () => {
     return(
-        <motion.div variants = {pageAnimation} initial = "hidden" animate = "show" exit = "exit">
-            <h1>Contact Us</h1>
-        </motion.div>
+        <ContactStyle
+            variants = {pageAnimation} 
+            initial = "hidden" 
+            animate = "show" 
+            exit = "exit"
+            style = {{ background: "#ffffff" }}
+        >
+            <Title>
+                <Hide>
+                    <motion.h2 variants = {titleAnimation}>Get in touch.</motion.h2>
+                </Hide>
+            </Title>
+            <div>
+                <Hide>
+                    <Social variants = {titleAnimation}>
+                        <Circle/>
+                            <h2>Sent Us A Message.</h2>
+                    </Social>
+                </Hide>
+                <Hide>
+                    <Social variants = {titleAnimation}>
+                        <Circle/>
+                            <h2>Sent Us E-mail.</h2>
+                    </Social>
+                </Hide>
+                <Hide>
+                    <Social variants = {titleAnimation}>
+                        <Circle/>
+                            <h2>Social Media.</h2>
+                    </Social>
+                </Hide>
+            </div>
+        </ContactStyle>
 
     )
 }
+const ContactStyle = styled(motion.div) ` 
+padding: 5rem 10rem;
+color: #353535;
+height: 90vh;
+`
+const Title = styled.div ` 
+color: black;
+margin-bottom: 4rem;
+`
+const Hide = styled.div ` 
+overflow: hidden;
+`
+const Circle = styled.div ` 
+width: 3rem;
+height: 3rem;
+background-color: #353535;
+border-radius: 50%;
+`
+const Social = styled(motion.div) ` 
+display: flex;
+align-items: center;
+h2{
+    margin: 2rem;
+}
+`
 
 export default ContactUs;
